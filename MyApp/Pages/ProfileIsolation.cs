1ba8043a5b54b1c41c9425b6f4b24b605b09d392
Zakir Hossain <planetofzakir@outlook.com>
2022-04-07 20:37:49 +0600

    Initial Create

---
+++ MyApp/Pages/ProfileIsolation.cs
@@ -0,0 +1,32 @@
+namespace MyApp.Pages
+{
+    public class ProfileIsolation
+    {
+    public const string PicContainerStyle="background-color: #1f1d2b";
+    public const string TextColor="color:white";
+    public const string AvatarStyle="height:90px; width:90px;";
+    public const string GridSpacing="margin-top:-70px;";
+    public const string TextAlign="margin-left: -40px;";
+    public const string ItemContainer="margin-top:-20px;";
+    public const string PaperStyle="background-color:#2D303E;color:white;";
+
+    public const string BottomContainer="position:fixed; bottom:0; left:auto; right:0; border-radius:20px 20px 0px 0px; background-color:#252836";
+    public const string HomePaperStyle="position:relative; bottom:10px; left:5px;";
+    public const string HomeIconStyle="color:#9A9CA6; display:relative; left:9px; color:#9A9CA6;";
+    public const string HomeTextStyle="font-size:12px; color:#9A9CA6;";
+    public const string SeriesIconStyle="color:#9A9CA6; display:relative; left:26px;";
+    public const string SeriesPaperStyle="position:relative; bottom:10px; left:22px;";
+    public const string SeriesTextStyle="font-size:12px;color:#9A9CA6;";
+    public const string VideoIcon=@"height:70px; width:70px; background-color:#1F1D2B;
+display:inline-block; border-radius: 50%; color:white;; 
+text-align: center; position:relative; bottom:50px;";
+public const string VideoIconPosition=@"height:52px; width:52px; background-color:#FF6600;
+display:inline-block; border-radius: 50%; color:white; padding: 8px; 
+text-align: center; position:relative; top:8px; z-index:20000000;";
+public const string ListsIcon="display:relative;right:50px; color:#9A9CA6;";
+public const string ListsPaper="position:relative; bottom:10px; right:50px; color:#9A9CA6;";
+public const string ListsText="font-size:12px;";
+public const string SearchIcon="display:relative;right:45px; color:#9A9CA6;";
+public const string SearchPaper="position:relative; bottom:10px; right:38px; color:#9A9CA6;";
+    }
+}
---
+++ MyApp/Shared/NavMenuIsolation.cs
@@ -0,0 +1,18 @@
+namespace MyApp.Shared
+{
+    public class NavMenuIsolation
+    {
+        
+     public  const string DrawerBgColor ="background-color:#1F1D2B; color:white;";
+     public  const string PaperStyle="background-color:#2D303E;color:white;";
+     public const string CloseIconStyle="background-color:#2D303E;color:white; cursor:pointer;";
+     public  const string PaperTextColor="color:white;";
+     public  const string IconColor="color:#828282";
+     public  const string ProfileLink="margin-bottom:-10px;";
+     public  const string ProfileText="margin-left:-40px;";
+     public  const string ListContainer="width:98%;";
+     public  const string ButtonStyle="position:absolute; bottom: -130px; left:-10px;";
+     public  const string SearchBox="height:42px; color:white;";
+   
+    }
+}
