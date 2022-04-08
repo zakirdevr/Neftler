089582a68fcbaa5487427f195f43cbc6fc4a360a
Zakir Hossain <planetofzakir@outlook.com>
2022-04-09 02:03:26 +0600

    Change Icon Style, Add Link Active Effect

--- MyApp/Pages/ProfileIsolation.cs
+++ MyApp/Pages/ProfileIsolation.cs
@@ -3,13 +3,15 @@ namespace MyApp.Pages
     public class ProfileIsolation
     {
     public const string PicContainerStyle="background-color: #1f1d2b";
-    public const string TextColor="color:white";
-    public const string AvatarStyle="height:90px; width:90px;";
+    public const string TextColor="color:white; font-size:16px;";
+    public const string AvatarStyle="height:88px; width:88px;";
     public const string GridSpacing="margin-top:-70px;";
     public const string TextAlign="margin-left: -40px;";
     public const string ItemContainer="margin-top:-20px;";
     public const string PaperStyle="background-color:#2D303E;color:white;";
 
+     public const string PaperRightIconStyle="color:white;font-size:11px;";
+     public const string LinkAvatarStyle="position:absolute;right:2px; top:2px;";
     public const string BottomContainer="position:fixed; bottom:0; left:auto; right:0; border-radius:20px 20px 0px 0px; background-color:#252836";
     public const string HomePaperStyle="position:relative; bottom:10px; left:5px;";
     public const string HomeIconStyle="color:#9A9CA6; display:relative; left:9px; color:#9A9CA6;";
@@ -17,6 +19,7 @@ namespace MyApp.Pages
     public const string SeriesIconStyle="color:#9A9CA6; display:relative; left:26px;";
     public const string SeriesPaperStyle="position:relative; bottom:10px; left:22px;";
     public const string SeriesTextStyle="font-size:12px;color:#9A9CA6;";
+    public const string ProfileIconStyle="color:#fa6602;font-size:11px;";
     public const string VideoIcon=@"height:70px; width:70px; background-color:#1F1D2B;
 display:inline-block; border-radius: 50%; color:white;; 
 text-align: center; position:relative; bottom:50px;";
@@ -28,5 +31,6 @@ public const string ListsPaper="position:relative; bottom:10px; right:50px; colo
 public const string ListsText="font-size:12px;";
 public const string SearchIcon="display:relative;right:45px; color:#9A9CA6;";
 public const string SearchPaper="position:relative; bottom:10px; right:38px; color:#9A9CA6;";
+
     }
 }
--- MyApp/Shared/NavMenuIsolation.cs
+++ MyApp/Shared/NavMenuIsolation.cs
@@ -5,14 +5,18 @@ namespace MyApp.Shared
         
      public  const string DrawerBgColor ="background-color:#1F1D2B; color:white;";
      public  const string PaperStyle="background-color:#2D303E;color:white;";
-     public const string CloseIconStyle="background-color:#2D303E;color:white; cursor:pointer;";
+     public const string CloseIconStyle="background-color:#2D303E;color:white; border-radius:7px; cursor:pointer;";
      public  const string PaperTextColor="color:white;";
      public  const string IconColor="color:#828282";
-     public  const string ProfileLink="margin-bottom:-10px;";
-     public  const string ProfileText="margin-left:-40px;";
+     public  const string ProfileNavLink="height:42px;";
+     public const string ProfileAvatar="position:absolute; left:16px;top:10px;";
+     public  const string ProfileText="position:absolute; left:53px;top:10px;";
+     public const string LinkAvatarStyle="position:absolute;right:2px; top:2px;";
      public  const string ListContainer="width:98%;";
      public  const string ButtonStyle="position:absolute; bottom: -130px; left:-10px;";
      public  const string SearchBox="height:42px; color:white;";
+     public const string PaperRightIconStyle="color:white;font-size:11px;";
+
    
     }
 }
